e370179cfa91b7a033c6664e46d4f02621b09b51
Matthew Koppe <mmmewk@gmail.com>
2018-10-17 15:55:40 -0700

    Fix url link

--- lib/apple/api.rb
+++ lib/apple/api.rb
@@ -1,12 +1,9 @@
 require 'apple/api/utils'
 require 'apple/api/search'
-require 'apple/music/api'
 
 module Apple
     module API
       include Apple::API::Utils
       include Apple::API::Search
-      include Apple::Music::API
-
     end
 end
--- lib/apple/music/client.rb
+++ lib/apple/music/client.rb
@@ -1,11 +1,14 @@
 require 'apple/client'
+require 'apple/music/api'
 
 module Apple
   module Music
     class Client
       include Apple::Client
+      include Apple::Music::API
+
       def url
-        "http://api.music.apple.com".freeze
+        "https://api.music.apple.com/".freeze
       end
     end
   end
