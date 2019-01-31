4eb57e290242811a4537abc52178b4b69d580ddd
Matthew Koppe <mmmewk@gmail.com>
2019-01-31 11:28:36 -0800

    remove outdated tests

--- spec/apple/books/api_spec.rb
+++
@@ -1,9 +0,0 @@
-RSpec.describe Apple::Books::Api do
-  it "has a version number" do
-    expect(Apple::Books::Api::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
-end
