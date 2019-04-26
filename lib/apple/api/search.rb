df1cb115ab9511445790f9a248a0c95fc235b9ca
Matthew Koppe <matthew@assembleinc.com>
2019-04-26 12:18:13 -0700

    make search not generate class objects unless specified

--- lib/apple/api/search.rb
+++ lib/apple/api/search.rb
@@ -7,13 +7,13 @@ module Apple
 
       def search(term, options={})
         res = perform_get("/v1/catalog/#{get_storefront}/search/", options.merge(term: term))
-        if res && res[:results]
-          res[:results].map do |type, hash|
-            {
-              type: type,
-              adams: generate_objects(type, hash[:data]),
-            }
-          end
+        return res unless options.generate_objects && res[:results]
+
+        res[:results].map do |type, hash|
+          {
+            type: type,
+            adams: generate_objects(type, hash[:data])
+          }
         end
       end
 
